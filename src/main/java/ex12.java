068d0da82dae4e9a63dd0b383b31f363c89e54dc
Phi-Hung Tran <Phi-Hung.phtran2002@gmail.com>
2021-09-13 10:41:10 -0700

    new

--- src/main/java/ex12.java
+++ src/main/java/ex12.java
@@ -5,30 +5,19 @@
 
 import java.util.Scanner;
 
-class Interest{
-    double principal;
-    double rate;
-    int years;
-    public void inputs(){
+public class ex12 {
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        double principal;
+        double rate;
+        int years;
         System.out.print("Enter the principal: ");
         principal = input.nextDouble(); //stores principal
         System.out.print("Enter the rate of interest: ");
         rate = input.nextDouble();  //stores rate
         System.out.print("Enter the number of years: ");
         years = input.nextInt();    //store years
-    }
-    public void outputs(){
         int value =(int)Math.round(principal*(1+rate*years*0.01));  //calculates interest
         System.out.println("After "+years+" years at "+rate+"%, the investment will be worth $"+value);
-    }
-}
-
-public class ex12 {
-    public static void main(String[] args){
-        Interest call = new Interest();
-        call.inputs();  //calls function inputs
-        call.outputs(); //calls function outputs
-
     }
 }
